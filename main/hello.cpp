c1da6e45ff2bfbac843ba77f97be34126cc1e52d
Amruta-L <amruta.lakkakula@gmail.com>
2025-03-10 10:50:19 +0000

    changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES2UG22CS063\n";
   cout << "I have successfully built and run this  \n";
